04bbebb8784615a9fabc2b38c45cebbf0a88493c
Clement COUTAREL <coutarel.clement.pro@gmail.com>
2024-03-14 20:37:20 -0500

    Implemented Title change and bottom modal layout

--- app/src/main/java/com/clementcoutarel/musicapp/MainView.kt
+++ app/src/main/java/com/clementcoutarel/musicapp/MainView.kt
@@ -3,15 +3,26 @@ package com.clementcoutarel.musicapp
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -20,6 +31,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.primarySurface
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,8 +44,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.InteroperableComposeUiNode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -38,7 +55,9 @@ import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class,
+    ExperimentalMaterialApi::class
+)
 @Composable
 fun MainView(){
 
@@ -61,72 +80,105 @@ fun MainView(){
         mutableStateOf(false)
     }
 
-    if (currentRoute != null) {
-        Log.d("route",currentRoute)
+    val isSheetFullScreen by remember {
+        mutableStateOf(false)
     }
+    val modifier = if(isSheetFullScreen) Modifier.fillMaxSize() else Modifier.fillMaxWidth()
+
+    val modalSheetState: ModalBottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = {it != ModalBottomSheetValue.HalfExpanded})
+
+    val roundedCornerRadius = if(isSheetFullScreen) 0.dp else 12.dp
 
     val bottomBar : @Composable () -> Unit = {
         if(currentScreen is Screen.DrawerScreen || currentScreen == Screen.BottomScreen.Home){
             BottomNavigation(modifier = Modifier.wrapContentSize()) {
                 screensInBottom.forEach {
                     item ->
+                    val tint = if(currentRoute == item.bRoute) Color.White else Color.Black
                     BottomNavigationItem(
                         selected = currentRoute == item.bRoute,
                         onClick = {
-                                  controller.navigate(item.bRoute)
+                          controller.navigate(item.bRoute)
+                            title.value = item.bTitle
                         },
                         icon = {
                             Icon(painter = painterResource(id = item.icon),
-                                contentDescription = item.bTitle )
+                                contentDescription = item.bTitle,
+                                tint=tint)
                         },
-                        label = { Text(text = item.bTitle)},
+                        label = { Text(text = item.bTitle, color = tint)},
                         selectedContentColor = Color.White,
-                        unselectedContentColor = Color.Black)
+                        unselectedContentColor = Color.Black
+                    )
                 }
             }
         }
     }
 
-    Scaffold(
-        bottomBar = bottomBar,
-        topBar = {
-            TopAppBar(title = { Text(text = title.value) },
-                navigationIcon = { IconButton(onClick = {
-                    scope.launch {
-                        scaffoldState.drawerState.open()
-                    }
-                }) {
-                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
-                }}
-            )
+    ModalBottomSheetLayout(
+        sheetContent = {
+            MoreBottomSheet(modifier = modifier)
         },
-        scaffoldState = scaffoldState,
-        drawerContent = {
-            LazyColumn(modifier = Modifier.padding(16.dp)) {
-                items(screensInDrawer){
-                    item ->
-                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+        sheetState = modalSheetState,
+        sheetShape = RoundedCornerShape(
+            topStart = roundedCornerRadius,
+            topEnd = roundedCornerRadius)
+        ) {
+        Scaffold(
+            bottomBar = bottomBar,
+            topBar = {
+                TopAppBar(title = { Text(text = title.value) },
+                    actions = {
+                        IconButton(onClick = {
+                              scope.launch {
+                                  if(modalSheetState.isVisible){
+                                      modalSheetState.hide()
+                                  }else{
+                                      modalSheetState.show()
+                                  }
+                              } }) {
+                          Icon(imageVector = Icons.Default.MoreVert,
+                              contentDescription = "Actions icon" )
+                      }
+                    },
+                    navigationIcon = { IconButton(onClick = {
                         scope.launch {
-                            scaffoldState.drawerState.close()
+                            scaffoldState.drawerState.open()
                         }
-                        if(item.dRoute == "add_account"){
-                            dialogOpen.value = true
-                        } else {
-                            controller.navigate(item.dRoute)
-                            title.value = item.dTitle
+                    }) {
+                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+                    }}
+                )
+            },
+            scaffoldState = scaffoldState,
+            drawerContent = {
+                LazyColumn(modifier = Modifier.padding(16.dp)) {
+                    items(screensInDrawer){
+                            item ->
+                        DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                            scope.launch {
+                                scaffoldState.drawerState.close()
+                            }
+                            if(item.dRoute == "add_account"){
+                                dialogOpen.value = true
+                            } else {
+                                controller.navigate(item.dRoute)
+                                title.value = item.dTitle
+                            }
                         }
                     }
                 }
             }
-        }
-        
-    ) {
-        Navigation(
-            navController = controller,
-            viewModel = viewModel,
-            pd = it )
+        ) {
+            Navigation(
+                navController = controller,
+                viewModel = viewModel,
+                pd = it )
 
-        AccountDialog(dialogOpen = dialogOpen)
+            AccountDialog(dialogOpen = dialogOpen)
+        }
     }
 }
 
@@ -150,6 +202,38 @@ fun DrawerItem(
         )
         Text(text = item.dTitle,
             style = MaterialTheme.typography.h5)
+    }
+}
 
+@Composable
+fun MoreBottomSheet(modifier: Modifier){
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(
+                MaterialTheme.colors.primarySurface
+            )
+        ){
+        Column(modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.SpaceBetween) {
+            Row(modifier = Modifier.padding(16.dp)) {
+                Icon(painter = painterResource(id = R.drawable.baseline_settings_24),
+                    contentDescription = "Settings",
+                    modifier = Modifier.padding(end=8.dp)
+                )
+                Text(text = "Settings",
+                    color = Color.White,
+                    fontSize = 20.sp)
+            }
+            Row(modifier = Modifier.padding(16.dp)) {
+                Icon(painter = painterResource(id = R.drawable.baseline_share_24),
+                    contentDescription = "Share",
+                    modifier = Modifier.padding(end=8.dp)
+                )
+                Text(text = "Share",
+                    color = Color.White,
+                    fontSize = 20.sp)
+            }
+        }
     }
 }
